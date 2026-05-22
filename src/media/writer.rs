ede4cd94838578a95aaa4c85f29fe95fc4e9e2f6
Lukas Herman <lherman.cs@gmail.com>
2026-05-22 18:36:47 +0800

    init abs-capture-time

--- src/media/writer.rs
+++ src/media/writer.rs
@@ -1,10 +1,11 @@
-use std::time::Instant;
+use std::time::{Instant, SystemTime};
 
 use crate::RtcError;
 use crate::format::PayloadParams;
 use crate::rtp_::MidRid;
 use crate::rtp_::VideoOrientation;
 use crate::session::Session;
+use crate::rtp::AbsCaptureTime;
 
 use super::{ExtensionValues, KeyframeRequestKind, Media, MediaTime, Mid, Pt, Rid, ToPayload};
 
@@ -94,6 +95,15 @@ impl<'a> Writer<'a> {
     /// whether the video requires to be rotated to show correctly.
     pub fn video_orientation(mut self, o: VideoOrientation) -> Self {
         self.ext_vals.video_orientation = Some(o);
+        self
+    }
+
+    /// Set absolute capture time for this packet.
+    pub fn abs_capture_time(mut self, capture_time: SystemTime) -> Self {
+        self.ext_vals.abs_capture_time = Some(AbsCaptureTime {
+            capture_time,
+            clock_offset: None,
+        });
         self
     }
 
